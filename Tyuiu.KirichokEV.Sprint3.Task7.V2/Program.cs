672cb21756f2b7057727d0c7150d4aa2b6770452
Elizaveta Kirichok <Tyuiu.KirichokEV@outlook.com>
2024-11-29 20:02:50 +0500

    Task7

---
+++ Tyuiu.KirichokEV.Sprint3.Task7.V2/Program.cs
@@ -0,0 +1,57 @@
+﻿using Tyuiu.KirichokEV.Sprint3.Task7.V2.Lib;
+
+namespace Tyuiu.KirichokEV.Sprint3.Task7.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Киричoк Е. В.| РППб-24-1";
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                         *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                          *");
+            Console.WriteLine("* Задание #0                                                                        *");
+            Console.WriteLine("* Вариант #18                                                                       *");
+            Console.WriteLine("* Выполнила: Киричок Елиавета Викторовна | РППб-24-1                                *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Написать прогарммму используя цикла for, которая вычисляет произведение ряда      *");
+            Console.WriteLine("* по формуле, при Х=1                                                               *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+
+
+            Console.WriteLine("+---------+------------+");
+            Console.WriteLine("|    X    |     f(x)   |");
+            Console.WriteLine("+---------+------------+");
+            for (int i = 0; i<=len-1; i++)
+            {
+                Console.WriteLine("|{0,5:d}    |   {1, 6:f2}   |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+---------+------------+");
+            Console.ReadKey();
+        }
+    }
+}
